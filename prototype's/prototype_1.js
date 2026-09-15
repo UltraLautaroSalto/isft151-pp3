57f49733d6d615e250016e710626199bee2f2568
UltraLautaroSalto <101681380+UltraLautaroSalto@users.noreply.github.com>
2026-09-15 20:57:41 -0300

    15/9/2026

--- prototype's/prototype_1.js
+++ prototype's/prototype_1.js
@@ -1,34 +1,69 @@
 let Cuentas_Registradas = [
-    {nombre: "Jorge", apellido: "Torres", edad: "16", genero: "Masculino", codigo_de_ingreso: "1234", rol: "Estudiante", Asistencia: "FALSE"},
-    {nombre: "Maria", apellido: "Gutierrez", edad: "15", genero: "Femenino", codigo_de_ingreso: "4321", rol: "Estudiante", Asistencia: "FALSE"},
-    {nombre: "Alex", apellido: "Santos", edad: "17", genero: "Sin Identificar", codigo_de_ingreso: "9999", rol: "Estudiante", Asistencia: "FALSE"},
+    {nombre: "Jorge", apellido: "Torres", edad: "16", genero: "Masculino", codigo_de_ingreso: "1234", rol: "Estudiante", 
+        asistencias:{"Algoritmo y Estructura de Datos 1": "FALSE", "Practicar Profecionalizantes 3": "FALSE", "Matematicas 2": "FALSE"}},
+    {nombre: "Maria", apellido: "Gutierrez", edad: "15", genero: "Femenino", codigo_de_ingreso: "4321", rol: "Estudiante", 
+        asistencias:{"Algoritmo y Estructura de Datos 1": "FALSE", "Practicar Profecionalizantes 3": "FALSE", "Matematicas 2": "FALSE"}},
+    {nombre: "Alex", apellido: "Santos", edad: "17", genero: "Sin Identificar", codigo_de_ingreso: "9999", rol: "Estudiante", 
+        asistencias:{"Algoritmo y Estructura de Datos 1": "FALSE", "Practicar Profecionalizantes 3": "FALSE", "Matematicas 2": "FALSE"}},
 
     {nombre: "Lance", apellido: "Vanze", edad:"34", genero: "Masculino", codigo_de_ingreso: "VanceLastDance9876", rol: "Docente"},
     {nombre: "Denise", apellido: "Robinson", edad:"45", genero: "Femenino", codigo_de_ingreso: "CJMYLOVE6789", rol: "Docente"},
+    {nombre: "Niko", apellido: "Bellic", edad:"54", genero:"Masculino", codigo_de_ingreso: "STIWTDFLTITVWLF", rol: "Docente"},
 
     {nombre: "Ralph", apellido: "Philips", edad:"53", genero: "Masculino", codigo_de_ingreso: "AM128894", rol: "Administrador"}
 ];
 
-const Cuerpo_Principal = document.getElementById("Cuerpo_Principal"); //Usar esto despues
-const Pagina_Inicial = document.getElementById("PaginaInicial"); // La primera pagina que se encuentra el usuario al entrar al proyecto
+let Clases_Actuales = [
+    {Carrera: "Analista de Sistemas", Materia: "Algoritmo y Estructura de Datos 1", Año: "1°", Docente_Asignado: "Lance Vance", ID: "1"},
+    {Carrera: "Analista de Sistemas", Materia: "Practicar Profecionalizantes 3", Año: "3°", Docente_Asignado: "Denise Robinson", ID: "2"},
+    {Carrera: "Analista de Sistemas", Materia: "Matematicas 2", Año: "2°", Docente_Asignado: "Niko Bellic", ID: "3"}
+];
 
+let Usuario_Actual = null;
+let Apelacion_Actual = "";
+
+const Pagina_Inicial = document.getElementById("PaginaInicial"); // La Pagina Principal
+
+// Botones Para Mostrar una Seccion especifica
 const MostrarInicioSesionUsuario = document.getElementById("Boton_Iniciar_Sesion_Usuario") // Funcion para controlar la visibilidad de "IniciarSesion_Usuario"
-const TextoBienvenida_Estudiante = document.getElementById("MensajeBienvenida_Estudiante");
+const MarcarPresente_Estudiante = document.getElementById("Boton_MarcarPresente_Estudiante"); // Seccion que se ocupa de mostrar la tabla de clases y le permite al usuario marcar presente en X Clase
 
+const Boton_MarcarPresente_Estudiante = document.getElementById("Boton_MarcarPresente_Estudiante"); // Boton que controla la visibilida de la seccion "Clases_Estudiante"
+const Boton_MandarApelacion_Estudiante = document.getElementById("Boton_MandarApelacion_Estudiante"); // Boton que controla la visibilidad de la seccion "Apelacion_Estudiante"
+
+// Mensajes
+const TextoBienvenida_Estudiante = document.getElementById("MensajeBienvenida_Estudiante"); // Funcion que muestra el texto de Bienvenida del Estudiante
+const TextoBienvenida_Docente = document.getElementById("MensajeBienvenida_Docente"); // Funcion que muestra el texto de Bienvenida del Docente
+const TextoBienvenida_Administrador = document.getElementById("MensajeBienvenida_Administrador"); // Funcion que muestra el texto de Bienvenida del Administrador
+
+// Funciones Principales
 const IniciarSesion_Usuario = document.getElementById("IniciarSesion") // Seccion donde ocurre el Inicio de Sesion del Usuario
-const Sesion_Estudiante = document.getElementById("Sesion_Estudiante")
+const Sesion_Estudiante = document.getElementById("Sesion_Estudiante") // Seccion de la Sesion del Estudiante
+const Sesion_Docente = document.getElementById("Sesion_Docente"); // Seccion de la Sesion del Docente
+const Sesion_Administrador = document.getElementById("Sesion_Administrador"); // Seccion de la Sesion del Administrador
+const SeleccionarClase_Estudiante = document.getElementById("SeleccionarClase_Estudiante"); // Funcion que se ocupa de procesar el presente en la clase seleccionada
+const Clases_Estudiante = document.getElementById("Clases_Estudiante"); // Seccion de las clases del estudiante 
+const Apelacion_Estudiante = document.getElementById("Apelacion_Estudiante"); // Seccion de las apelaciones del estudiante
+const EnviarApelacion_Estudiante = document.getElementById("Boton_MandarApelacion_Estudiante"); // Seccion que se ocupa de las apelaciones enviadas por los estudiantes (estas se tendrian que almacenar en la base de datos para que despues el profe pueda verlas)
 
-const NM_Usuario = document.getElementById("NM_Usuario"); // Constante que almacena el Nombre del Usuario
+// Datos Almacenados en Memoria
+const NM_Usuario = document.getElementById("NM_Usuario"); // Constante que almacena el Nombre del Usuario 
 const CDG_Usuario = document.getElementById("CDG_Usuario"); // Constante que almacena la Contraseña del Usuario
 
+// Funciones Adicionales
 const Boton_Confirmar_Usuario = document.getElementById("Boton_Confirmar_Usuario"); // Boton que confirma la identidad del usuario
-
+const Boton_MarcarPresente = document.getElementById("Boton_MarcarPresente"); // Boton que envia el presente a la clase asignada
+const Boton_EnviarApelacion = document.getElementById("Boton_EnviarApelacion"); // Boton que envia la apelacion al docente asignado a la clase
 const Boton_Volver = document.querySelectorAll(".Opcion_Volver"); // Funcion que controla el volver a la pagina anterior
 
 const Secciones = [
     Pagina_Inicial,
     IniciarSesion_Usuario,
-    Sesion_Estudiante
+    Sesion_Estudiante,
+    Sesion_Docente,
+    Sesion_Administrador,
+    Clases_Estudiante,
+    Apelacion_Estudiante
 ];
 
 let Historial_Secciones = [];
@@ -55,7 +90,26 @@ function MostrarSeccion(section) {
 
 //Funcion para Mostrar la Seccion de Inicio de Sesion
 MostrarInicioSesionUsuario.addEventListener("click", () => {
-    MostrarSeccion(IniciarSesion_Usuario);
+    if(Usuario_Actual === null){
+        MostrarSeccion(IniciarSesion_Usuario);
+    }
+    switch(Usuario_Actual.rol) {
+        case "Estudiante":
+        MostrarSeccion(Sesion_Estudiante);
+        break;
+
+        case "Docente":
+            MostrarSeccion(Sesion_Docente);
+        break;
+
+        case "Administrador":
+        MostrarSeccion(Sesion_Administrador);
+        break;
+
+        default:
+            console.log("Rol no Reconocido");
+        break;
+    }
 });
 
 // Funcion del Boton para confirmar el Inicio de Sesion del Usuario
@@ -82,25 +136,69 @@ Boton_Confirmar_Usuario.addEventListener("click", () => {
         return;
     }
 
+    Usuario_Actual = Identificar_US;
+
     if (Identificar_US.rol === "Estudiante") {
         MostrarSeccion(Sesion_Estudiante);
         MostrarBienvenidaEstudiante(Identificar_US);
     }
 
     if(Identificar_US.rol === "Docente"){
-        console.log("Tipo de Usuario Docente");
+        MostrarSeccion(Sesion_Docente);
+        MostrarBienvenidaDocente(Identificar_US);
     }
 
     if(Identificar_US.rol === "Administrador"){
-        console.log("Tipo de Usuario Administrador");
+        MostrarSeccion(Sesion_Administrador);
+        MostrarBienvenidaAdministrador(Identificar_US);
     }
 });
 
 function MostrarBienvenidaEstudiante(usuario_actual){
     TextoBienvenida_Estudiante.innerText =
-        `Bienvenido: ${usuario_actual.nombre} ${usuario_actual.apellido}
-            Rol: ${usuario_actual.rol}`;
+        `Bienvenido: ${usuario_actual.nombre} ${usuario_actual.apellido}`;
 }
+
+function MostrarBienvenidaDocente(usuario_actual){
+    TextoBienvenida_Docente.innerText = 
+        `Bienvenido: ${usuario_actual.nombre} ${usuario_actual.apellido}`;
+}
+
+function MostrarBienvenidaAdministrador(usuario_actual){
+    TextoBienvenida_Administrador.innerText = 
+        `Bienvenido: ${usuario_actual.nombre} ${usuario_actual.apellido}`;
+}
+
+function CargarClases(){
+    Clases_Actuales.forEach(clase => {
+        const opcion = document.createElement("option");
+        opcion.value = clase.Materia;
+        opcion.textContent = clase.Materia;
+        SeleccionarClase_Estudiante.appendChild(opcion);
+    });
+}
+
+Boton_MarcarPresente_Estudiante.addEventListener("click", () => {
+    CargarClases();
+    MostrarSeccion(Clases_Estudiante);
+});
+
+Boton_MarcarPresente.addEventListener("click", () => {
+    const Clase_Seleccionada = SeleccionarClase_Estudiante.value;
+
+    if (Clase_Seleccionada === "") {
+        alert("Seleccione una clase");
+        return;
+    }
+
+    Usuario_Actual.asistencias[Clase_Seleccionada] = "TRUE";
+    Mensaje_Asistencia.innerText =`${Usuario_Actual.nombre} ${Usuario_Actual.apellido} ha sido marcado como PRESENTE en: ${Clase_Seleccionada}`;
+});
+
+Boton_MandarApelacion_Estudiante.addEventListener("click", () => {
+    CargarClases();
+    MostrarSeccion(Apelacion_Estudiante);
+});
 
 function VolverSeccion() {
     if (Historial_Secciones.length === 0) {
